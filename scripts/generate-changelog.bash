741743f2e0ad56321a97bddc3c306ba7b11917e6
Akihiro Chida <95008746+a-chida@users.noreply.github.com>
2025-01-17 09:43:55 +0000

    feat: add script for generating changelogs and update package.json

---
+++ scripts/generate-changelog.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Get tag name
+tag=$1
+
+# sample command
+sample_command="sample: npm run generate-changelog -- v1.0.0"
+
+# validate tag name
+if [ -z "$tag" ]; then
+  echo "Error: Tag name is required"
+  echo $sample_command
+  exit 1
+fi
+
+# semver regex
+semver_regex="^v[0-9]+\.[0-9]+\.[0-9]+$"
+
+# validate tag name with semver regex
+if [[ ! $tag =~ $semver_regex ]]; then
+  echo "Error: Invalid tag name. "
+  echo $sample_command
+  exit 1
+fi
+
+# get the latest tag in semantic version format.
+latest_tag=$(git tag --sort=-v:refname | grep -E $semver_regex | head -n 1)
+
+if [ -z "$latest_tag" ]; then
+  echo "Error: Not found any tag in semantic version format."
+  exit 1
+fi
+
+# create output directory
+output_dir="blog/changelogs"
+mkdir -p $output_dir
+
+# create changelog file
+npm exec git-cliff -- --tag $tag --output $output_dir/CHANGELOG-$tag.md $latest_tag..HEAD
